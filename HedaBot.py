8592d8f1871ef56724ff543e0e09458cbc8b3c5b
Scratchcat1 <awinniecat@live.com>
2017-10-17 16:05:57 +0000

    Update HedaBot.py

--- HedaBot.py
+++ HedaBot.py
@@ -439,7 +439,7 @@ def CreateCommandDictionary():
                 
         
 
-BOT_TOKEN = "472230564:AAEHTSJ446LE_BO_hQ8B4PeVmUTrB8gRsEA"
+BOT_TOKEN = "YOUR TOKEN HERE"
 if __name__ == "__main__":
     bot = telepot.Bot(BOT_TOKEN)
     heda = Telebot(bot)
